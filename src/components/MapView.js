5f45da171354445ab56b27006fe6dfd64b9f73ad
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-05-13 22:03:24 +0300

    now Returns coordinates+address
    Map selection now Converts geocode to Address and returns coordinates + address selected

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import NominatimGeocoder from "nominatim-geocoder";
 
-const Map = ({ onCoordinatesChange }) => {
+const MapView = ({ onCoordinatesChange }) => {
+  const geoConvert = new NominatimGeocoder();
+  const [address, setAddress] = useState("");
   let map;
   let marker;
 
   useEffect(() => {
     map = L.map("map").setView([51.505, -0.09], 13);
 
-    L.tileLayer("https://{s}.tile.jawg.io/5a646c26-4702-40b1-8fed-671eacbf1892/{z}/{x}/{y}{r}.png?access-token=Jsz7VZAnkb84aX0p5Oq8HwK57Vu4YmeRlNf1t7TUaujVsv3eOgqX8IWMoeUQ5DRU", {
-      attribution: "<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors",
-    }).addTo(map);
+    L.tileLayer(
+      "https://{s}.tile.jawg.io/5a646c26-4702-40b1-8fed-671eacbf1892/{z}/{x}/{y}{r}.png?access-token=Jsz7VZAnkb84aX0p5Oq8HwK57Vu4YmeRlNf1t7TUaujVsv3eOgqX8IWMoeUQ5DRU",
+      {
+        attribution:
+          '<a href="https://www.jawg.io" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
+      }
+    ).addTo(map);
 
     const handleClick = (e) => {
       const { lat, lng } = e.latlng;
-      console.log(lat, lng);
+      //console.log(lat, lng);
 
       // Remove the previously added marker from the map
       if (marker) {
@@ -32,8 +39,24 @@ const Map = ({ onCoordinatesChange }) => {
         }),
       }).addTo(map);
 
-      // Call the callback function with the clicked coordinates
-      onCoordinatesChange({ lat, lng });
+      // Call the callback function with the clicked coordinates and the address
+      geoConvert.reverse({ lat, lon: lng }).then((response) => {
+        const city =
+          response.address.city ||
+          response.address.town ||
+          response.address.village;
+        const country = response.address.country;
+        let clickedAddress = "";
+        if (country === "Israel") {
+          clickedAddress = `${city}, Palestine`;
+        } else {
+          clickedAddress = `${city}, ${country}`;
+        }
+        setAddress(clickedAddress);
+        onCoordinatesChange({ lat, lng, address: clickedAddress });
+        console.log(lat, lng, clickedAddress);
+      });
+      
     };
 
     map.on("click", handleClick);
@@ -50,4 +73,4 @@ const Map = ({ onCoordinatesChange }) => {
   return <div id="map" style={{ height: "400px" }}></div>;
 };
 
-export default Map;
+export default MapView;
