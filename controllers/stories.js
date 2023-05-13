616d2c64382cb1e19f58858cf95cbb3875a3ba90
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-05-13 21:57:16 +0300

    Fix Search, Added Tags...
    Fixed Search and Filtering stories, added Tags and Address

--- controllers/stories.js
+++ controllers/stories.js
@@ -12,21 +12,67 @@ authRouter.use(cookieSession({
 
 export const getStories = (req, res) => {
   const q = req.query.userid 
-    ? "SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, s.uid, `year`, `postdate`, `geocode` FROM users u JOIN stories s ON u.id = s.uid WHERE s.uid = ?"
-    : req.query.year || req.query.title 
-      ? "SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, s.uid, `year`, `postdate`, `geocode` FROM users u JOIN stories s ON u.id = s.uid WHERE year=? OR title LIKE ? "
-      : "SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, `year`, `postdate`, `geocode` FROM users u JOIN stories s ON u.id = s.uid";
+    ? 'SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, s.uid, `year`, `postdate`, `geocode`, `address`, `tags`, `likes` FROM users u JOIN stories s ON u.id = s.uid WHERE s.uid = ?'
+    : buildFilterQuery(req.query);
 
-  db.query(q, [req.query.userid || req.query.year, `%${req.query.title}%`], (err, data) => {
+  const params = [];
+
+  if (req.query.userid) {
+    params.push(req.query.userid);
+  } else {
+    if (req.query.year) {
+      params.push(`%${req.query.year}%`);
+    }
+
+    if (req.query.title) {
+      params.push(`%${req.query.title}%`);
+    }
+
+    if (req.query.tags) {
+      params.push(`%${req.query.tags}%`);
+    }
+  }
+
+  db.query(q, params, (err, data) => {
     if (err) return res.status(500).send(err);
 
     return res.status(200).json(data);
   });
 };
 
+
+////filter
+const buildFilterQuery = ({ year, title, tags }) => {
+  let filterQuery = 'SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, s.uid, `year`, `postdate`, `geocode`, `address`, `tags`, `likes` FROM users u JOIN stories s ON u.id = s.uid';
+  let filters = [];
+
+  if (year) {
+    filters.push('year LIKE ?');
+  }
+
+  if (title) {
+    filters.push('title LIKE ?');
+  }
+
+  if (tags) {
+    filters.push('tags LIKE ?');
+  }
+
+  if (filters.length > 0) {
+    filterQuery += ' WHERE ' + filters.join(' OR ');
+  }
+
+  return filterQuery;
+};
+
+
+
+
+
+
 export const getStory = (req, res) => {
   const q =
-    "SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, `year`, `postdate`, `geocode`, `uid` FROM users u JOIN stories s ON u.id = s.uid WHERE s.id = ? ";
+    "SELECT s.id, `username`, `title`, `content`, s.img, u.img AS userImg, `year`, `postdate`, `geocode`, `address`, `tags`, `likes`, `uid` FROM users u JOIN stories s ON u.id = s.uid WHERE s.id = ? ";
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
 
@@ -56,39 +102,32 @@ export const getUserStories = (req, res) => {
 };
 
 export const postStory = (req, res) => {
- /* const token = req.cookies.geomemToken;
-  if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");*/
+     const q =
+       "INSERT INTO stories(`title`, `content`, `img`, `postdate`, `year`, `geocode`, `address`, `tags`, `likes`, `uid`) VALUES (?)";
+ 
+     const values = [
+       req.body.title,
+       req.body.content,
+       req.body.img,
+       req.body.postdate,
+       req.body.year,
+       req.body.geocode,
+       req.body.address,
+       req.body.likes,
+       JSON.stringify(req.body.tags),
+       req.body.uid,
+     ];
+ 
+     db.query(q, [values], (err, data) => {
+       if (err) return res.status(500).json(err);
+       return res.json("Post has been created.");
+     });
+ };
 
-    const q =
-      "INSERT INTO stories(`title`, `content`, `img`, `postdate`, `year`, `geocode`, `uid`) VALUES (?)";
-
-    const values = [
-      req.body.title,
-      req.body.content,
-      req.body.img,
-      req.body.postdate,
-      req.body.year,
-      req.body.geocode,
-      req.body.uid,
-    ];
-
-    db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.json("Post has been created.");
-    });
- // });
-};
 
 export const deleteStory = (req, res) => {
 
- // const token = req.session.geomemToken;
- // if (!token) return res.status(401).json("Not authenticated ! "+token);
-
-//  jwt.verify(token, "jwtkey", (err, userInfo) => {
- //   if (err) return res.status(403).json("Token not valid ! "+token);
 
     const storyid = req.params.id;
     const q = "DELETE FROM stories WHERE `id` = ?";
@@ -106,9 +145,9 @@ export const updateStory = (req, res) => {
 
     const storyId = req.params.id;
     const q =
-      "UPDATE stories SET `title`=?,`content`=?,`img`=?,`year`=?,`geocode`=? WHERE `id` = ?";
+      "UPDATE stories SET `title`=?,`content`=?,`img`=?,`year`=?,`geocode`=? ,`address`=?, `tags`=? WHERE `id` = ?";
 
-    const values = [req.body.title, req.body.content, req.body.img, req.body.year, req.body.geocode];
+    const values = [req.body.title, req.body.content, req.body.img, req.body.year, req.body.geocode, req.body.address, req.body.tags];
 
     db.query(q, [...values, storyId], (err, data) => {
       if (err) return res.status(500).json(err);
