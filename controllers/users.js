5f07a0aa7eb44cea451e4538f9da37525a383d56
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-05-13 21:57:31 +0300

    small changes

--- controllers/users.js
+++ controllers/users.js
@@ -10,8 +10,8 @@ export const getUsers = (req, res) => {
 
 
   const q = req.query.name 
-  ? "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `address` FROM users WHERE username LIKE ?":
-  "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `address` FROM users ";
+  ? "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `country` FROM users WHERE username LIKE ?":
+  "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `country` FROM users ";
   db.query(q,[`%${req.query.name}%`],(err, data) => {
     if (err) {
       console.error(err);
@@ -29,9 +29,9 @@ export const updateUser = (req, res) => {
 
     const userId = req.params.id;
     const q =
-      "UPDATE users SET `bio`=?,`birthdate`=?,`img`=?,`address`=? WHERE `id` = ?";
+      "UPDATE users SET `bio`=?,`birthdate`=?,`img`=?,`country`=? WHERE `id` = ?";
 
-    const values = [req.body.bio, req.body.birthdate, req.body.img, req.body.address];
+    const values = [req.body.bio, req.body.birthdate, req.body.img, req.body.country];
 
     db.query(q, [...values, userId], (err, data) => {
       if (err) return res.status(500).json(err);
@@ -46,7 +46,7 @@ export const getUser = (req, res) => {
       return res.status(400).json( "Missing id parameter");
     }
   
-    const q = "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `address` FROM users WHERE id = ?";
+    const q = "SELECT `id`, `username`, `img`, `bio`, `birthdate`, `posts`, `followers`, `following`, `country` FROM users WHERE id = ?";
     db.query(q, [id], (err, data) => {
       if (err) {
         console.error(err);
