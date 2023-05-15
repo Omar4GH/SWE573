2e805ce992fe90ecca0adbed0d4a6bf6005d533f
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-05-15 17:38:11 +0300

    Created landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-import logo from '../assets/LogoGeoMemoirs.png';
-
+import logo from '../assets/MiniLogoGeoMemoirs.png';
+import { Link } from "react-router-dom";
 
 function Home() {
     
@@ -10,10 +10,26 @@ function Home() {
  
 
     return (
-<div className="flex flex-wrap justify-center items-center h-screen ">
-Home
-<img src={logo} />
-</div>
+        <div className="flex flex-col items-center justify-center ">
+        <img src={logo} alt="Logo" className="h-96" />
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          Welcome to GeoMemoirs!
+        </h1>
+        <div className="text-center">
+          <p className="text-lg text-gray-700 mb-8">
+            Discover the world through the memories of others with Geomemoir. 
+            Immerse yourself in unique stories and experiences shared by users from all over the globe, each one tied to a specific location on the map. 
+            Create your own memory and leave your mark on the world, or explore the stories of others and connect with people through shared experiences. 
+            With Geomemoir, the world is at your fingertips.
+          </p>
+        </div>
+        <Link to="/feed">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Start Browsing
+          </button>
+        </Link>
+      </div>
+      
     );
   
 
