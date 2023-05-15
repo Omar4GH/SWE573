9b047b51e43552ad969c84aca1b31d31e16e18db
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-05-15 17:15:20 +0300

    Connect Online Database
    Credentials hidden in .env file

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const db = mysql.createConnection({
-host:"localhost",
-port: 6400,
-database:"livingstories",
-user:"root",
-password:"password",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 
 
 })
